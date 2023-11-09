478c7f058ae6b04e655e87e603a0e2e27669c2e7
khoiphamrtyfgh <qbkhoi2311@gmail.com>
2023-11-09 15:54:08 +0700

    Khôi

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -44,7 +44,7 @@ class RegisterController extends Controller
             Auth::login($user);
 
             return redirect('/');
-
+            
         } else {
             return back()->withInput()->withErrors(['password_confirmation' => 'Password confirmation Không trùng khớp']);
         }

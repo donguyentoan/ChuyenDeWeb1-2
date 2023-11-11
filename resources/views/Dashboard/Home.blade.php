fcd705ebea1a5c6a1100e5a778291c8a960c8b0f
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2023-11-11 07:37:38 +0700

    update

--- resources/views/Dashboard/Home.blade.php
+++ resources/views/Dashboard/Home.blade.php
@@ -1,166 +1,177 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>K-WD Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="build/css/tailwind.css"/>
-    <script src="https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@0.5.x/dist/component.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="build/css/tailwind.css" />
+    <script src="https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@0.5.x/dist/component.min.js">
+    </script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
-    
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
-  </head>
+</head>
+
 <body>
 
-<div class="div flex">
+    <div class="div flex">
 
- <div class="w-1/4">
- @include('component.SideBar')
- </div>
+        <div class="w-1/4">
+            @include('component.SideBar')
+        </div>
 
- <div class="w-full overflow-hidden rounded-lg shadow-xs">
-              <div class="w-full overflow-x-auto">
+        <div class="w-full overflow-hidden rounded-lg shadow-xs">
+            <div class="w-full overflow-x-auto">
                 <div class="button_add flex justify-end mr-3">
-                <a href=""><button class="flex  mx-auto  text-white bg-gradient-to-r from-cyan-500 to-blue-500 border-0 py-2 px-9 m-5  rounded text-xs">Add Product</button></a>
+                    <a href=""><button
+                            class="flex  mx-auto  text-white bg-gradient-to-r from-cyan-500 to-blue-500 border-0 py-2 px-9 m-5  rounded text-xs">Add
+                            Product</button></a>
                 </div>
 
                 <div class="container">
-                <div class="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 py-5">
-    <div class="w-full max-w-3xl">
-        <div class="-mx-2 md:flex">
-            <div class="w-full md:w-1/3 px-2">
-                <div class="rounded-lg shadow-sm mb-4">
-                    <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
-                        <div class="px-3 pt-8 pb-10 text-center relative z-10">
-                            <h4 class="text-sm uppercase text-gray-500 leading-tight">Users</h4>
-                            <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">3,682</h3>
-                            <p class="text-xs text-green-500 leading-tight">▲ 57.1%</p>
+                    <div class="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 py-5">
+                        <div class="w-full max-w-3xl">
+                            <div class="-mx-2 md:flex">
+                                <div class="w-full md:w-1/3 px-2">
+                                    <div class="rounded-lg shadow-sm mb-4">
+                                        <div
+                                            class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                                            <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                                                <h4 class="text-sm uppercase text-gray-500 leading-tight">Users</h4>
+                                                <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">
+                                                    3,682</h3>
+                                                <p class="text-xs text-green-500 leading-tight">▲ 57.1%</p>
+                                            </div>
+                                            <div class="absolute bottom-0 inset-x-0">
+                                                <canvas id="chart1" height="70"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="w-full md:w-1/3 px-2">
+                                    <div class="rounded-lg shadow-sm mb-4">
+                                        <div
+                                            class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                                            <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                                                <h4 class="text-sm uppercase text-gray-500 leading-tight">Subscribers
+                                                </h4>
+                                                <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">
+                                                    11,427</h3>
+                                                <p class="text-xs text-red-500 leading-tight">▼ 42.8%</p>
+                                            </div>
+                                            <div class="absolute bottom-0 inset-x-0">
+                                                <canvas id="chart2" height="70"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="w-full md:w-1/3 px-2">
+                                    <div class="rounded-lg shadow-sm mb-4">
+                                        <div
+                                            class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                                            <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                                                <h4 class="text-sm uppercase text-gray-500 leading-tight">Comments</h4>
+                                                <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">
+                                                    8,028</h3>
+                                                <p class="text-xs text-green-500 leading-tight">▲ 8.2%</p>
+                                            </div>
+                                            <div class="absolute bottom-0 inset-x-0">
+                                                <canvas id="chart3" height="70"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="absolute bottom-0 inset-x-0">
-                            <canvas id="chart1" height="70"></canvas>
+                    </div>
+                    <!-- BUY ME A BEER AND HELP SUPPORT OPEN-SOURCE RESOURCES -->
+                    <div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+                        <div>
+                            <a title="Buy me a beer" href="https://www.buymeacoffee.com/scottwindon" target="_blank"
+                                class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+                                <img class="object-cover object-center w-full h-full rounded-full"
+                                    src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg" />
+                            </a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="w-full md:w-1/3 px-2">
-                <div class="rounded-lg shadow-sm mb-4">
-                    <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
-                        <div class="px-3 pt-8 pb-10 text-center relative z-10">
-                            <h4 class="text-sm uppercase text-gray-500 leading-tight">Subscribers</h4>
-                            <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">11,427</h3>
-                            <p class="text-xs text-red-500 leading-tight">▼ 42.8%</p>
-                        </div>
-                        <div class="absolute bottom-0 inset-x-0">
-                            <canvas id="chart2" height="70"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full md:w-1/3 px-2">
-                <div class="rounded-lg shadow-sm mb-4">
-                    <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
-                        <div class="px-3 pt-8 pb-10 text-center relative z-10">
-                            <h4 class="text-sm uppercase text-gray-500 leading-tight">Comments</h4>
-                            <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">8,028</h3>
-                            <p class="text-xs text-green-500 leading-tight">▲ 8.2%</p>
-                        </div>
-                        <div class="absolute bottom-0 inset-x-0">
-                            <canvas id="chart3" height="70"></canvas>
-                        </div>
-                    </div>
+                <div
+                    class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+                    <span class="flex items-center col-span-3">
+                        Showing 21-30 of 100
+                    </span>
+                    <span class="col-span-2"></span>
+                    <!-- Pagination -->
+                    <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+                        <nav aria-label="Table navigation">
+                            <ul class="inline-flex items-center">
+                                <li>
+                                    <button
+                                        class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
+                                        aria-label="Previous">
+                                        <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                            <path
+                                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                                clip-rule="evenodd" fill-rule="evenodd"></path>
+                                        </svg>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                        1
+                                    </button>
+                                </li>
+                                <li>
+                                    <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                        2
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                        3
+                                    </button>
+                                </li>
+                                <li>
+                                    <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                        4
+                                    </button>
+                                </li>
+                                <li>
+                                    <span class="px-3 py-1">...</span>
+                                </li>
+                                <li>
+                                    <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                        8
+                                    </button>
+                                </li>
+                                <li>
+                                    <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                        9
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple"
+                                        aria-label="Next">
+                                        <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                            <path
+                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                clip-rule="evenodd" fill-rule="evenodd"></path>
+                                        </svg>
+                                    </button>
+                                </li>
+                            </ul>
+                        </nav>
+                    </span>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+</body>
 
-
-<!-- BUY ME A BEER AND HELP SUPPORT OPEN-SOURCE RESOURCES -->
-<div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
-    <div>
-        <a title="Buy me a beer" href="https://www.buymeacoffee.com/scottwindon" target="_blank" class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
-            <img class="object-cover object-center w-full h-full rounded-full" src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg"/>
-        </a>
-    </div>
-</div>
-                </div>
-
-                
-          
-              <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
-                <span class="flex items-center col-span-3">
-                  Showing 21-30 of 100
-                </span>
-                <span class="col-span-2"></span>
-                <!-- Pagination -->
-                <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
-                  <nav aria-label="Table navigation">
-                    <ul class="inline-flex items-center">
-                      <li>
-                        <button class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple" aria-label="Previous">
-                          <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
-                            <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
-                          </svg>
-                        </button>
-                      </li>
-                      <li>
-                        <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                          1
-                        </button>
-                      </li>
-                      <li>
-                        <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                          2
-                        </button>
-                      </li>
-                      <li>
-                        <button class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">
-                          3
-                        </button>
-                      </li>
-                      <li>
-                        <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                          4
-                        </button>
-                      </li>
-                      <li>
-                        <span class="px-3 py-1">...</span>
-                      </li>
-                      <li>
-                        <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                          8
-                        </button>
-                      </li>
-                      <li>
-                        <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
-                          9
-                        </button>
-                      </li>
-                      <li>
-                        <button class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple" aria-label="Next">
-                          <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
-                            <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
-                          </svg>
-                        </button>
-                      </li>
-                    </ul>
-                  </nav>
-                </span>
-              </div>
-            </div>
-    
-
-          </div>
-
-
-
-
-
-          </body>
 </html>

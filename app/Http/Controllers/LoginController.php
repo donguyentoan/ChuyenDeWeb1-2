011924514b24e3b295683176e6bc6d3967c40900
donguyentoan2003 <donguyentoan2003@gmail.com>
2023-11-11 08:13:39 +0700

    loginfix

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -23,10 +23,10 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        // $request->validate([
-        //     'email' => 'required|email|min:8|max:50',
-        //     'password' => 'required|min:8|max:50'
-        // ]);
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
         $remember = $request->filled('remember');
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials, $remember)) {
@@ -43,47 +43,6 @@ class LoginController extends Controller
         return view('Login.forgot-password');
     }
 
-
-    public function sendResetLinkEmail(Request $request)
-    {
-        $request->validate(['email' => 'required|email']);
-
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
-        return $status === Password::RESET_LINK_SENT
-            ? back()->with(['status' => __($status)])
-            : back()->withErrors(['email' => __($status)]);
-    }
-
-    //  reset
-    public function showResetForm(Request $request, $token)
-    {
-        return view('Login.reset-password', ['token' => $token, 'email' => $request->email]);
-    }
-
-    public function reset(Request $request)
-    {
-        $request->validate([
-            'token' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|confirmed|min:8',
-        ]);
-
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user, $password) {
-                $user->forceFill([
-                    'password' => Hash::make($password)
-                ])->save();
-                $user->setRememberToken(Str::random(60));
-                event(new User($user));
-            }
-        );
-        return $status === Password::PASSWORD_RESET
-            ? redirect()->route('login')->with(['status' => __($status)])
-            : back()->withErrors(['email' => __($status)]);
-    }
     //login with gg
     public function redirectToGoogle()
     {

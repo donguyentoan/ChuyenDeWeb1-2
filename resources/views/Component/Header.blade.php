f0eb56b1c3d18e03a2e1ad0a130aa34c8a356d7a
20211tt2476Thuy <lethuy.sota@gmail.com>
2023-11-28 17:08:43 +0700

    test automation function login

--- resources/views/Component/Header.blade.php
+++ resources/views/Component/Header.blade.php
@@ -72,9 +72,7 @@
     </div>
     <nav
         class="md:w-11/12 overflow-x-auto w-full mx-auto py-2 mb-3 bg-[#00603c] md:rounded-xl md:ml-auto flex items-center text-white flex-row flex md:text-base text-sm ">
-        @forEach($categories as $ct)
-        <a class="mr-5 ml-5 hover:text-gray-900">{{$ct->name}}</a>
-        @endforeach
+       
         <a class="mr-5 hover:text-gray-900">Salad</a>
         <a class="mr-5 hover:text-gray-900">Thức Uống</a>
         <a class="mr-5 hover:text-gray-900">Mỳ Ý</a>
